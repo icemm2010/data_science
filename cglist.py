f44fa95023434c460df119338a10ce56648007b7
Yanping <yanpingyin@gmail.com>
2016-10-27 22:46:24 +0800

    source code

---
+++ cglist.py
@@ -0,0 +1,10 @@
+#cglist.py
+
+#for sale advertisements on craigslist
+
+#build database for all the for sale items
+
+#give seller a suggested price if they have something to put on craigslist for sale
+#(price may change depending on the season)
+
+#notify buyer by email once the wanted item is posted for sale on craigslist
